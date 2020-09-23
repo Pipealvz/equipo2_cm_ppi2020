1bfc4517c2ce4e733a0c38236e4d070062f55e1b
Felipe <pipedim03@gmail.com>
2020-09-22 20:51:03 -0500

    Carpeta Backend y Frontend

---
+++ Backend/Index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req,res){
+    res.send('Hola mundo');
+});
+app.listen(5057, function(){
+    console.log("El servidor está en uso.");
+});
