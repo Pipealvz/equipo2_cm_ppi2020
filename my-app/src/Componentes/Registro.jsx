5e4454f55f728224165847950daa1937f57fbd5b
Felipe <pipedim03@gmail.com>
2020-09-24 19:35:31 -0500

    Cambios para derly

---
+++ my-app/src/Componentes/Registro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo_domiapp from '../Images_proyectos/';
+class Registro extends React.Component {
+        render() {
+                return (
+                        <div className="Logo_domiApp">
+                                <img src={Logo_domiapp} className=""> </img>
+                                <p>Hola</p>
+                        </div>
+                )
+        }
+}
+
+export default Registro;
