4a7afb7beb6c94c9df5310cdaa26f8571e23f925
Felipe <pipedim03@gmail.com>
2020-10-02 10:56:51 -0500

    Cambios de las ventanas Inicio y registro

--- FrontEnd/my-app/src/Componentes/Solicitud.jsx
+++ FrontEnd/my-app/src/Componentes/Solicitud.jsx
@@ -1,54 +1,31 @@
 import React from 'react';
-import '../Estilos/App.css';
+import '../Estilos/Solicitud.css';
 import Logo_domiapp from '../Images_proyectos/Logo_domiApp.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-class Inicio_sesion extends React.Component {
+class Solicitud extends React.Component {
         render() {
                 return (
                         <div className="App">
-                                <Navbar titulo="Registro" />
-                                <div className="container vh-100 mt-4">
-                                        <div className="logo_domiapp">
-                                                <img src={Logo_domiapp} alt="" className="img-fluid" />
-                                        </div>
+                                <Navbar titulo="Solicitudes" />
+                                <div className="container vh-100">
                                         <div className="row">
                                                 <div className="col"></div>
-                                                <div className="col-8" id="Bloque_inicio">
-                                                        <h1 className="text-center mt-4" id="Texto_inicio">Iniciar Sesión</h1>
+                                                <div className="mt-1 col-10" id="Bloque_solicitud">
+                                                        <h1 className="text-center mt-4" id="Texto_inicio">Solicitudes</h1>
                                                         <br />
-                                                        <form >
-                                                                <div className="form-group">
-                                                                        <label className="inputEmail">Correo</label>
-                                                                        <input
-                                                                                type="email"
-                                                                                name="email"
-                                                                                id="inputEmail"
-                                                                                className="form-control"
-                                                                                required
-                                                                                placeholder="Correo"
-                                                                        />
-                                                                </div>
-                                                                <div className="form-group ">
-                                                                        <label className="inputPassword">Contraseña</label>
-                                                                        <input
-                                                                                type="password"
-                                                                                name="contrasena"
-                                                                                id="inputPassword"
-                                                                                className="form-control"
-                                                                                required
-                                                                                placeholder="Contraseña"
-                                                                        />
-                                                                </div>
-                                                                <button className=" btn btn-primary btn-block" type="submit" id="btn_inicio">
-                                                                        <strong>Iniciar Sesión</strong>
+                                                        <div class="dropdown">
+                                                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                                        Dropdown button
                                                                 </button>
-                                                                <button className=" btn btn-primary btn-block" type="submit" id="btn_registro">
-                                                                        <strong>Registrarse</strong>
-                                                                </button>
-                                                        </form>
+                                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                                        <a class="dropdown-item" href="#">Action</a>
+                                                                        <a class="dropdown-item" href="#">Another action</a>
+                                                                        <a class="dropdown-item" href="#">Something else here</a>
+                                                                </div>
+                                                        </div>
                                                         <br />
                                                         <h6></h6>
                                                         <br />
@@ -61,4 +38,4 @@ class Inicio_sesion extends React.Component {
                 );
         }
 }
-export default Inicio_sesion;
+export default Solicitud;
--- FrontEnd/my-app/src/index.js
+++ FrontEnd/my-app/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Inicio_sesion from './Componentes/Inicio_sesion.jsx';
 import Registro from './Componentes/Registro';
+import Solicitud from './Componentes/Solicitud';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Registro />
+    <Solicitud />
   </React.StrictMode>,
   document.getElementById('root')
 );
