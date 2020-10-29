3b281bfeb2d3109cb2969440895585d4c371688d
Felipe <pipedim03@gmail.com>
2020-10-29 02:27:08 -0500

    Efectos Inicio y registro

--- FrontEnd/my-app/src/Componentes/Registro.jsx
+++ FrontEnd/my-app/src/Componentes/Registro.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 /*IMPORT-COMPONENTS*/
 import Footer from './Footer';
 import '../Estilos/Registro.css';
-import Logo_domiapp from '../Images_proyectos/Logo_domiApp.png';
 /*IMPORT-BOOTSTRAP*/
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,42 +17,43 @@ class Registro extends React.Component {
                             <h1 className="text-center mt-4" id="Texto_registro">Registro</h1>
                             <hr />
                             <form >
-                                <div className="form-group">
-                                    <label className="inputName">Nombre de usuario</label>
+                                <div className="form-group" id="form-group">
+                                    <label className="inputName" id="form-label">Nombre de usuario</label>
                                     <input
                                         type="text"
                                         name="Nombre"
                                         id="input_Name"
                                         className="form-control"
                                         required
-                                        placeholder="Nombre"
+                                        placeholder=""
                                     />
                                 </div>
-                                <div className="form-group">
-                                    <label className="inputEmail">Correo</label>
+                                <div className="form-group" id="form-group">
+                                    <label className="inputEmail" id="form-label">Correo</label>
                                     <input
                                         type="email"
                                         name="email"
                                         id="input_Email"
                                         className="form-control"
                                         required
-                                        placeholder="Correo"
+                                        placeholder=""
                                     />
                                 </div>
-                                <div className="form-group ">
-                                    <label className="inputPassword">Contraseña</label>
+                                <div className="form-group" id="form-group">
+                                    <label className="inputPassword" id="form-label">Contraseña</label>
                                     <input
                                         type="password"
                                         name="contrasena"
                                         id="input_Password"
                                         className="form-control"
                                         required
-                                        placeholder="Contraseña"
+                                        placeholder=""
                                     />
                                 </div>
-                                <button className=" btn btn-block mt-0" type="submit" id="btn_registrar">
+                                <button className="btn btn-block mt-4" type="submit" id="btn_registrar">
                                     <strong>Crear cuenta</strong>
                                 </button>
+                                <hr/>
                                 <div className="Link_sesion">
                                     <p>¿Ya tienes cuenta? <Link id="link" to="/">
                                         ¡¡Inicia Sesión!!</Link>
