f5a993ee69f5d693134846669386fe39a57c820d
Felipe Álvarez Uribe <Pipedim03@gmail.com>
2020-11-19 14:22:43 +0000

    Ocultar credenciales

--- BackEnd/db/mysql_pool.js
+++ BackEnd/db/mysql_pool.js
@@ -2,10 +2,11 @@ const util = require('util')
 var mysql = require('mysql');
 var mysqlConnection  = mysql.createPool({
   connectionLimit: 10,
-  host:"btmpb7wisqt3ydcps71x-mysql.services.clever-cloud.com",
-  user:"ujyyb1uctp6mprlo",
-  password:"LVXeoiYtEC7FQ4W16zhd",
-  database:"btmpb7wisqt3ydcps71x"
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE,
+  multipleStatements: true
 })
 
 mysqlConnection.on('relase', (connection)=>{
