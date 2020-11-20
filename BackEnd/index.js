a8b69240460d52dc955827a55767b9a13dd0fb85
Felipe <pipedim03@gmail.com>
2020-11-20 11:14:41 -0500

    replit 3

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,8 +23,8 @@ app.use("/api", require("./routes/Pedido"))
 app.use("/api", require("./routes/Usuario_cliente"))
 app.use("/api", require("./routes/historial"))
 
-app.set('port',process.env.PORT || 5000);
+app.set('port',process.env.port || 5000);
 
-app.listen(app.get("puerto"), () => {
-  console.log(`Servidor corriendo en el puerto ${app.get("puerto")}`)
+app.listen(app.get("port"), () => {
+  console.log(`Servidor corriendo en el puerto ${app.get("port")}`)
 })
