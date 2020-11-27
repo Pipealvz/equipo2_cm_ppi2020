6aa3fedd70fb68d4eeac8397f03eb339bb23970b
Guerralowsky <ALEJANDROGUERRAORREGO11@gmail.com>
2020-11-26 23:12:26 -0500

    Add welcome

--- FrontEnd/my-app/src/web/web.jsx
+++ FrontEnd/my-app/src/web/web.jsx
@@ -19,19 +19,21 @@ export default class web extends React.Component{
             <img src={logo_domiapp}  className="img-icon"/>
     </div>
     <div className="row col-6 justify-content-end">
-        <button className="btn btn-primary" id="btn">Iniciar sesión</button>
-        <div className="mr-5"></div>
-        <button className="btn btn-primary" id="btn">Registro</button>
+        <span className="text-uppercase text-white">¡Para tí, siempre. DomiApp!</span>
     </div>
     </nav>
     <div className="container-fluid" id="bg-web">
         <div className="row-fluid d-flex justify-content-center" id="entrada">
-            <span className="align-self-center text-uppercase" id="spantitle">Bienvenidos a la web de <img src={logo_domiapp}/> </span>
+            <span className="align-self-center text-uppercase" id="spantitle">Conoce la web de <img src={logo_domiapp}/> </span>
+            <div className="mt-5"></div>
         </div>
+        <span className="align-self-center text-uppercase" id="spantitle">¡Bienvenido!</span>
     </div>
     <div className="mt-2"></div>
     <div className="container-fluid" id="bg-web2">
-
+    <div className="row-fluid d-flex justify-content-center bg-dark" id="entrada">
+        
+    </div>
     </div>
     </div>
     )
