31c02d741bdd64642e1a30ec33940725a23e4293
Juan Jose <2631425juan@gmail.com>
2020-11-27 01:08:05 -0500

    Login farmacia

--- FrontEnd/my-app/src/Farmacias/Solicitud.jsx
+++ FrontEnd/my-app/src/Farmacias/Solicitud.jsx
@@ -6,10 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 /*IMPORT-COMPONENTS*/
 import Footer from './Footer';
 import Navbar from './Navbar-btn';
+import Card from "../Componentes/card";
+import data from "../data/card.json";
 /*IMPORT-IMAGES*/
-import btn_form from '../Images_proyectos/btn-add-formula.png';
+import formula from '../'
 
 class Solicitud extends React.Component {
+        constructor() {
+                super();
+                this.state = {
+                    data: data
+                };
+            }
         render() {
                 return (
                         <div className="App-s">
@@ -17,10 +25,13 @@ class Solicitud extends React.Component {
                                 <div className="mt-5"></div>
                                 <div className="container">
                                         <div className="row mb-5 justify-content-center">
-                                                        <div className="col-12 col-lg-5 align-self-start" id="Bloque_solicitud">
+                                                        <div className="col-12 col-lg-11 align-self-start" id="Bloque_solicitud">
                                                                 <h1 className="text-center" id="Texto_solicitud">Pedidos</h1>
                                                                 <hr />
                                                                 <div className="mt-5"></div>
+                                                                <div>
+                                                                <Card className="text" time="Fecha: 12/02/2019" farm="Usuario: Androly-San" data={this.state.data} />
+                                                                </div>
                                                                 <h6></h6>
                                                                 <br />
                                                         </div>
---
+++ FrontEnd/my-app/src/Farmacias/login_farm.jsx
@@ -0,0 +1,91 @@
+/*IMPORT-REACT*/
+import React,{useState} from 'react';
+import { Link, useHistory } from 'react-router-dom'; //IMPORT useHistory
+/*IMPORT-BOOTSTRAP*/
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*IMPORT-COMPONENTS*/
+import '../Estilos/Inicio_sesion.css';
+/*IMPORT-IMAGES*/
+import logo_domiapp from '../Images_proyectos/Logo_domiApp.png';
+/*IMPORT AXIOS { GET, POST, PUT, DELETE }*/
+import axios from 'axios'
+
+
+export default function Inicio_sesion() {
+
+        const history = useHistory(); //DEFINIMOS useHistory
+        const [correo, setCorreo] = useState("");
+        const [clave, setPassword] = useState("");
+        const eventoRegistrar = (e)=>{
+        e.preventDefault();
+        const loginUser = {
+                correo:correo,
+                contraseña:clave
+        }
+        axios.post('https://equipo2cmppi2020-2.felipealvarez8.repl.co/api/farmacia',loginUser)
+                .then(()=> {
+                        if(eventoRegistrar === correo,clave){
+                                history.push("Solicitud");
+                        }
+                        alert('Lo siento, algo anda mal :/')
+                })
+                .catch((error)=>{
+                        console.log(error)
+                })
+        }
+        return (
+                <div className="App">
+                        <div className="container vh-100" id="container">
+                                <div className="row vh-100 justify-content-end">
+                                        <div className="col-12 col-lg-4 align-self-center" id="Bloque_inicio">
+                                                <img src={logo_domiapp} className="img-fluid" />
+                                                <h1 className="text-center mt-4" id="Texto_inicio">Iniciar Sesión como farmacia</h1>
+                                                <hr />
+                                                <br />
+                                                <form onSubmit={eventoRegistrar} method="post" name="Loginform">
+                                                        <div className="form-group">
+                                                                <input
+                                                                        type="email"
+                                                                        name="correo"
+                                                                        id="i-email"
+                                                                        className="form-control"
+                                                                        required
+                                                                        placeholder="correo"
+                                                                        onChange={function({target}){
+                                                                                setCorreo(target.value);
+                                                                        }}
+                                                                />
+                                                                <label for="name" className="form-label">Correo</label>
+                                                                <br />
+                                                                <input
+                                                                        type="password"
+                                                                        name="contrasena"
+                                                                        id="i-pass"
+                                                                        className="form-control"
+                                                                        required
+                                                                        placeholder="contraseña"
+                                                                        onChange={function({target}){
+                                                                                setPassword(target.value);
+                                                                        }}
+                                                                />
+                                                                <label for="password" className="form-label">Contraseña</label>
+                                                        </div>
+                                                        <Link id="link" to="/Solicitud_f">
+                                                                <button className=" btn btn-dark btn-block mt-0"  type="submit" id="btn_inicio">
+                                                                        <strong>Iniciar sesión</strong>
+                                                                </button>
+                                                                <hr />
+                                                        </Link>
+                                                        <span className="link-registro">¿No estás registrado como farmacia? <Link id="link" to="/Registro.farmacia">¡Registrate!</Link>
+                                                        </span>
+                                                        <div className="row"></div>
+                                                        <span className="link-farmacia">¿Eres un usuario? <Link id="link" to="/">¡Inicia sesión!</Link>
+                                                        </span>
+                                                </form>
+                                                <br />
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        );
+}
