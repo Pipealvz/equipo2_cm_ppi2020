e5ec8816e26d69fa10fab01f7d4e2ba95347da8d
Guerralowsky <ALEJANDROGUERRAORREGO11@gmail.com>
2020-11-26 21:18:13 -0500

    Inicio de la web

--- FrontEnd/my-app/src/web/web.jsx
+++ FrontEnd/my-app/src/web/web.jsx
@@ -4,7 +4,7 @@ import { Link, useHistory } from 'react-router-dom'; //IMPORT useHistory
 /*IMPORT-BOOTSTRAP*/
 import 'bootstrap/dist/css/bootstrap.min.css';
 /*IMPORT-COMPONENTS*/
-import '../Estilos/Inicio_sesion.css';
+import './web.css';
 /*IMPORT-IMAGES*/
 import logo_domiapp from '../Images_proyectos/Logo_domiApp.png';
 /*IMPORT AXIOS { GET, POST, PUT, DELETE }*/
@@ -13,20 +13,22 @@ import axios from 'axios'
 export default class web extends React.Component{
     render(){
     return (
-        <div className="Appweb">
-            <div className="container vh-100">
-                <nav class="navbar navbar-light align-self-center sticky-top bg-inverse" id="navbar">
-                    <div class="row col-6">
-                        <a class="navbar-brand">
-                            <img src="../Images_proyectos/Logo_domiApp-icon.png" alt=""/>
-                        </a>
-                        <div class="row col-6 justify-content-end">
-                            <button class="btn btn-primary" id="btn">Iniciar sesión</button>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
+        <div className="appweb">
+        <nav class="navbar navbar-light align-self-center sticky-top bg-inverse" id="navbarweb">
+        <div class="row col-6">
+            <img src={logo_domiapp}  className="img-icon"/>
+    </div>
+    <div class="row col-6 justify-content-end">
+        <button class="btn btn-primary" id="btn">Iniciar sesión</button>
+    </div>
+    </nav>
+    <div className="container-fluid bg-primary" id="bg-web">
+
+    </div>
+    <div className="container-fluid" id="bg-web2">
+
+    </div>
+    </div>
     )
 }
 }
